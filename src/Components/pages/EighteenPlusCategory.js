06880a0796272251a349d257f3979444926e64c1
Ishwari18 <ishwarirele18@.com>
2023-10-25 19:04:37 +0530

    category stuff
    users in a category get rendered 18+

--- src/Components/pages/EighteenPlusCategory.js
+++ src/Components/pages/EighteenPlusCategory.js
@@ -1,11 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-const EighteenPlusCategory = ({ category }) => (
-  <div>
-    <h2>{category.name}</h2>
-    {/* Add any 18+ category-specific details here */}
-    <div>this is 18+ stuff</div>
-  </div>
-);
+const EighteenPlusCategory = ({ category }) => {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const categoryId = category._id; // Replace with your actual category ID
+    const fetchUsersInCategory = async () => {
+      try {
+        const response = await fetch(`http://localhost:5000/api/categories/${categoryId}`);
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        setUsers(data);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching users in the 18+ category:", error);
+        setLoading(false);
+      }
+    };
+
+    fetchUsersInCategory();
+  }, [category._id]);
+
+  return (
+    <div>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          <h2>{category.name}</h2>
+          {/* Add any 18+ category-specific details here */}
+          <div>this is 18+ stuff</div>
+          <h3>Users Selling Services in this 18+ Category:</h3>
+          <ul>
+            {users.map((user) => (
+              <li key={user._id}>{user.name}</li>
+            ))}
+          </ul>
+        </>
+      )}
+    </div>
+  );
+};
 
 export default EighteenPlusCategory;
